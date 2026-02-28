1e2438efb4b9679cee5b507044a7104ec4d92f25
ja154 <jmwanguwe3@gmail.com>
2026-02-28 15:24:34 +0300

    anonymous classes part

---
+++ Person.java
@@ -0,0 +1,11 @@
+public abstract class Person{ // abstract class
+
+    abstract void eat(); // abstract method(without implementation)
+
+    // concrete method(with implementation)
+    public void  speak(){
+        System.out.println("Jaba language");
+    }
+
+
+}
--- Test.java
+++ Test.java
@@ -1,13 +1,13 @@
 public class Test{  // main class // driver class
 public static void main(String[] args) {
-	
-	Student s1 = new Student();  // creating an object - Student
-	s1.setStudent(123,"ABC", 8.9);
-	s1.getStudent(); // all the student details
 
-	// sname
-    s1.getSname();
+	Person p = new Person(){ //process instantiation + class definatoion anonymous class.
 
-
+		void eat(){
+			System.out.println("Eating Jaba");
+		} 
+	};
+	p.eat();
+	p.speak();
 }
-}
+} 
