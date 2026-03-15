739a712e249dc611211b14d1cd34de3ab6841a6a
ja154 <jmwanguwe3@gmail.com>
2026-03-15 05:59:15 +0300

    add ExceptionDemo class for basic arithmetic operations

---
+++ ExceptionDemo.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+
+        int res = a / b;
+        System.out.println("Result = " + res);
+
+        int prod = a * b;
+        System.out.println("Product = " + prod);
+
+        System.out.println("Rest of the code follows ");
+
+    }
+
+}
