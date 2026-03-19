e7b4bbfb5c66936aa2f3e0d396474cf44f66f6a5
ja154 <jmwanguwe3@gmail.com>
2026-03-19 18:52:13 +0300

    exception handling 2 actual enforcement of try and catch blocks using the superclass Exception.

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -1,18 +1,30 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-
+        System.out.println("Enter 2 numbers");
+        int a = 0, b = 0;
+        try{
+        a = in.nextInt();
+        b = in.nextInt();
+    
         int sum = a + b;
         System.out.println("Sum = " + sum);
 
+
+        //enforcing a try catch block for arithmetic exceptions eg 10/0 which can throw an exception error
+
+        
         int res = a / b;
         System.out.println("Result = " + res);
-
+        }
+        catch(Exception e){
+            //e.printStackTrace();
+            System.err.println(e);
+        }
         int prod = a * b;
         System.out.println("Product = " + prod);
 
