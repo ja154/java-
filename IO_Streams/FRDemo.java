b7629f201e46bb75367c71911822e6c5cee88003
ja154 <jmwanguwe3@gmail.com>
2026-03-29 14:58:27 +0300

    file io first run with actual code to practicve with both read and write opperations on files in java.

--- IO_Streams/FRDemo.java
+++ IO_Streams/FRDemo.java
@@ -1,4 +1,20 @@
 package IO_Streams;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FRDemo {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("First.txt");
+        //reads a char and returns the int equivalent
+        // EOF - - 1
+        //we loop through the file printing each character by equating it to x thus the loop will print each character until it reads the last character
+        int x;
+        while((x = fr.read()) != -1){
+            System.out.print((char)x);
+
+        }
+        fr.close();
+    }
 }
---
+++ IO_Streams/FWDemo.java
@@ -0,0 +1,22 @@
+
+package IO_Streams;
+import java.io.*;
+
+public class FWDemo {
+    public static void main(String[] args) throws IOException {// we have to declare the IO Exception as we are using classes within java IO package
+
+        //File f = new File("First.txt");// created if exists it is replaced
+        FileWriter fw = new FileWriter("first.txt", true); //appending - true we are appending to an existing file 
+
+            fw.write("java is some jaba and jaba is java\n");
+            fw.write("jaba is fresh and unpropergated\n");
+            fw.write ("Nimeja ngori na nakuambia ni mbaya\n");
+            fw.write("Huskii jaba ni java gathee\n");
+            fw.write("Nakuambia ni mbaya\n");
+            fw.write("Nyonga hio tenje\n");
+
+            System.out.println("file printed nyonga hio tenje......");
+
+            fw.close();
+    }
+}
